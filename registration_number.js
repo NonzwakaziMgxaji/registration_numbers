8a727a53dfe9afcfe5517dfee4c74be44dab3efa
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.comgit config --listgit config --global user.email nonzwakazimgxaji@gmail.com>
2021-05-19 10:20:41 +0200

    changes to registration_numbers

--- registration_number.js
+++ registration_number.js
@@ -1,27 +1,134 @@
-// var regNumEntered = document.querySelector(".RegTextbox");
-// var addBtn = document.querySelector(".addBtn");
-// var radioBtnSelected = document.querySelector(".radio");
-// var showBtn = document.querySelector(".showBtn");
-// var regNumSpan = document.querySelector(".showRegNum");
+var regNumEntered = document.querySelector(".RegTextbox");
+var addBtn = document.querySelector(".addBtn");
+var radioBtnSelected = document.querySelector(".radio");
+var showBtn = document.querySelector(".showBtn");
+var regNumSpan = document.querySelector(".showRegNum");
+var error = document.querySelector(".errorMsg");
+var showAll = document.querySelector(".showAllBtn");
+var reset = document.querySelector(".resetBtn")
 
-// var regNumberEntered;
+var regNumLocal = {};
 
-// if (localStorage['regNumbers']) {
-//     regNumberEntered = JSON.parse(localStorage.getItem('regNumbers'));
+// var displayRegNumbers = []
+
+if (localStorage["regNumbers"]) {
+    regNumLocal = JSON.parse(localStorage.getItem("regNumbers"));
+}
+
+var regPlateInstance = regNumbersFactory();
+
+// regPlateInstance.inputReg(regNumLocal);
+
+// function regNum() {
+//     var getRegNum = regNumEntered.value;
+//     getRegNum = getRegNum.toUpperCase();
+
+//     return  getRegNum;
 // }
 
-// addBtn.addEventListener("click", function () {
 
-//     var registrationNumber = regNumSpan.value;
+addBtn.addEventListener("click", function () {
+    let regNumber = regNumEntered.value;
+    if (regPlateInstance.storedReg(regNumber)) {
+
+        localStorage.setItem('regNumbers', JSON.stringify(regPlateInstance.showStoredReg()));
+
+        let spanElem = document.createElement("li");
+
+        regNumSpan.appendChild(spanElem);
+        spanElem.innerHTML = regNumber;
+
+        if (regNumLocal[regNumber] > 0) {
+            // console.log()
+            error.innerHTML = "This registration number has already been entered!"
+        } else {
+            regNumSpan.appendChild(spanElem);
+            spanElem.innerHTML = regNumber;
+        }
+
+    } else {
+        error.innerHTML = "Enter a valid registration number using format shown in textbox!"
+    }
+
+    regNumEntered.value = "";
+
+
+});
+
+
+
+showBtn.addEventListener("click", function () {
+    // let clearExisting = regNumSpan.children;
+    // for (let i = clearExisting.length -1; i>= 0; i--){
+    //     regNumSpan.removeChild(clearExisting[i]);
+    // }
+
+// let emptyArray =[];
+    var rad = document.querySelector("input[name='town']:checked");
+    if (rad) {
     
+        let spanElem = document.createElement("li");
+        localStorageData = Object.keys(regNumLocal);
+        for (let i = 0; i < localStorageData.length; i++) {
+            let regNombolo = localStorageData[i];
+            if (radioBtnSelected.value === "capetown") {
+                if (regNombolo.startsWith("CA")) {
+                    let emptyArray =[];
+                    emptyArray.push(regNombolo);
+                    for( let i = 0; i < emptyArray.length; i++){
+                        let spanElem = document.createElement("li");
+                        spanElem.innerHTML =emptyArray[i];
+                        regNumSpan.appendChild(spanElem);
+                    }
+                }
+            } else if (radioBtnSelected.value === "bellville") {
+                if (regNombolo.startsWith("CY")) {
+                    let emptyArray =[];
+                    emptyArray.push(regNombolo);
+                    for( let i = 0; i < emptyArray.length; i++){
+                        let spanElem = document.createElement("li");
+                        spanElem.innerHTML =emptyArray[i];
+                        regNumSpan.appendChild(spanElem);
+                    }
+                }
+            } else if (radioBtnSelected.value === "malmesbury") {
+                if (regNombolo.startsWith("CK")) {
+                    let emptyArray=[];
+                    emptyArray.push(regNombolo);
+                    for( let i = 0; i < emptyArray.length; i++){
+                        let spanElem = document.createElement("li");
+                        spanElem.innerHTML =emptyArray[i];
+                        regNumSpan.appendChild(spanElem);
+                    }
+                }
+            }
+            else if (radioBtnSelected.value === "stellenbosch"){
+                if (regNombolo.startsWith("CL")) {
+                    let emptyArray =[];
+                    emptyArray.push(regNombolo);
+                    for( let i = 0; i < emptyArray.length; i++){
+                        let spanElem = document.createElement("li");
+                        spanElem.innerHTML =emptyArray[i];
+                        regNumSpan.appendChild(spanElem);
+                    }
+                }
+            } 
+        }
+    }
+});
 
-//     localStorage.setItem('regNumbers', JSON.stringify());
+showAll.addEventListener("click", function () {
+    var localStorageData = JSON.parse(localStorage.getItem("regNumbers"));
+    // document.getElementById("showRegNumID").innerHTML = "";
 
-// });
+    for (var i = 0; i < localStorageData.length; i++) {
+        localStorageData = Object.keys(regNumLocal);
+        document.getElementById("showRegNumID").innerHTML += localStorageData[i];
+    }
+});
 
-// function xyz() {
-//     var abc = document.createElement("button");
-//     abc.innerHTML = "HI";
-//     document.body.appendChild(abc);
-// }
 
+reset.addEventListener('click', function () {
+    localStorage.clear();
+    location.reload();
+});
--- registration_number_factory.js
+++ registration_number_factory.js
@@ -1,11 +1,40 @@
-// function regNumbers(existingRegNumbers){
+function regNumbersFactory(){
 
-//     var regNumbersEntered = existingRegNumbers || {};
+    var regNumbersEntered = {};
+    var countRegNumbers = 0;
 
+    function storedReg(regPlate) {
+        
+        regPlate = regPlate.toUpperCase();
+        var regex = /^((CA|CY|CK|CL)\s\d{3}\-\d{3})$|^((CA|CY|CK|CL)\s\d{3}\d{3})$|^((CA|CY|CK|CL)\s\d{3}\s\d{3})$/;
+        var testRegularExp = regex.test(regPlate)
 
-//     function 
+        if (regNumbersEntered[regPlate] === undefined) {
+            regNumbersEntered[regPlate] = 0;
+            countRegNumbers++;
+        } else {
+            regNumbersEntered[regPlate]++;
+        }
 
+        return testRegularExp;
+    }
 
+    function inputReg(regNum) {
+        regNumbersEntered = regNum;
+    }
 
+    function showStoredReg() {
+        return regNumbersEntered;
+    }
 
-// }
+    function numOfReg() {
+        return countRegNumbers
+    }
+
+    return{
+        storedReg,
+        inputReg,
+        showStoredReg,
+        numOfReg,
+    }
+}
